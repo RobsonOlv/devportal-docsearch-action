c858066ee8219fbd193d28ee1b5073de1a6b9708
Robson Oliveira <ropj@cin.ufpe.br>
2022-11-28 19:59:42 -0300

    chore: fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,39 +10,21 @@ git clone https://github.com/vtexdocs/docsearch-scraper.git
 
 cd docsearch-scraper/
 
-# install pipenv without cache
-# pip install --no-cache-dir --trusted-host pypi.python.org pipenv
+# install pipenv
 pip3 install pipenv==2018.11.26
-
-# install packages without virtualenv
-# pipenv install --system --deploy --ignore-pipfile
-
-# python3 -m pip install python-dotenv
-# export "/usr/local/lib/python3.6/site-packages/"
 
 
 # create the .env file for docsearch
 echo "APPLICATION_ID=${APPLICATION_ID}
 API_KEY=${API_KEY}
 CHROMEDRIVER_PATH=/usr/bin/chromedriver
-CONFIG=config_md.json
 " > .env
-
-value=`cat .env`
-echo "$value"
 
 PIPENV_VENV_IN_PROJECT=true pipenv install
 
 echo "Update webclient.py"
 cp ./utils/webclient.py ./.venv/lib/python3.6/site-packages/scrapy/core/downloader/
 
-pipenv run ./docsearch run ./config_md.json
-# pipenv shell
-# ./docsearch
-
-
-
+pipenv run ./docsearch run $FILE
 
 echo "🚀 Successfully indexed and uploaded the results to Algolia"
-
-# exec python docsearch run $GITHUB_WORKSPACE/$FILE
