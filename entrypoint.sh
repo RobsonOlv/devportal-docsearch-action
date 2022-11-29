35a0458bfe58de8a1a9bab3a2f79617ff42102d5
Robson Oliveira <ropj@cin.ufpe.br>
2022-11-28 22:42:01 -0300

    chore: fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,11 @@ pip3 install pipenv==2018.11.26
 
 # download chromedriver
 
-# wget -q https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip
+wget -q https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip
 
-# unzip chromedriver_linux64.zip
-# chown root:root chromedriver
-# chmod +x chromedriver
+unzip chromedriver_linux64.zip
+chown root:root chromedriver
+chmod +x chromedriver
 
 echo "$PWD"
 ls
@@ -27,7 +27,7 @@ ls
 # create the .env file for docsearch
 echo "APPLICATION_ID=${APPLICATION_ID}
 API_KEY=${API_KEY}
-CHROMEDRIVER_PATH=/usr/bin/chromedriver
+CHROMEDRIVER_PATH=/github/workspace/docsearch-scraper/chromedriver
 " > .env
 
 PIPENV_VENV_IN_PROJECT=true pipenv install
